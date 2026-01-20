6c80f8a8f8b2e05084fc9d7ab3cb642e79b26ee1
yatharth1444 <yatharthsingh1444@gmail.com>
2026-01-20 17:27:24 +0530

    improvements in messages

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,11 +1,11 @@
 import express from 'express'
-import {getAllUsers} from '../controllers/message.controller.js'
+import {getAllUsers, getChatPartners, sendMessage, getMessageByUserId} from '../controllers/message.controller.js'
 import { protectRoute } from '../middlewares/authentication.middleware.js'
 import {arcjetProtection} from "../middlewares/arcjet.middleware.js"
 const router = express.Router()
 router.use(arcjetProtection, protectRoute)
 router.get('/contacts', getAllUsers)
-router.get('/chats', getChatPartners())
-router.get('/:id', getMessagesByUserId)
+router.get('/chats', getChatPartners)
+router.get('/:id', getMessageByUserId)
 router.post('/send/:id', sendMessage)
 export default router
